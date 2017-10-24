a8753865f0826114194716830ba48a19577d1d48
161710162 <161710162@smkassalaambandung.sch.id>
2017-10-24 11:27:05 +0400

    perubahan data

--- ular1.php
+++ ular1.php
@@ -2,8 +2,8 @@
 
 require_once 'ular.php';
 
- $ular=new robot('sssssts',2);
-echo"suara ular :".$ular->suara.'<br>';
-echo"berat ular :".$ular->berat.'<br>';
+ $ular1=new ular('sssssts',2);
+echo"suara ular :".$ular1->suara.'<br>';
+echo"berat ular :".$ular1->berat;
 
 ?>
